7f19d10e1e15eaf8d6076e447fb57568eeee889a
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:16:08 +0300

    Create InputBox.cs

---
+++ SQL_Speed_Work/SQL_Speed_Work/Dialogs/InputBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQL_Speed_Work.Dialogs
+{
+    public partial class InputBox : Form
+    {
+        public string Caption = "";
+        public string Value = "";
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void InputBox_Load(object sender, EventArgs e)
+        {
+            l_Caption.Text = Caption;
+        }
+
+        private void bOK_Click(object sender, EventArgs e)
+        {
+            Value = tB_Value.Text;
+            Close();
+        }
+
+        private void bCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
