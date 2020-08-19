9a6145e8369472061f395742895ef7178440be4b
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 14:34:04 +0300

    Create DataSet1.cs

---
+++ DB_Scructure_And_Objects/DB_Scructure_And_Objects/DataSet1.cs
@@ -0,0 +1,23 @@
+﻿namespace DB_Scructure_And_Objects
+{
+
+
+    partial class DataSet1
+    {
+        partial class FindResultDataTable
+        {
+        }
+
+        partial class LinksDataTable
+        {
+        }
+
+        partial class TablesDataTable
+        {
+        }
+
+        partial class FieldsDataTable
+        {
+        }
+    }
+}
