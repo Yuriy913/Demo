7bf4ed5cc8879d7968e3c30b2d339f1037faee2a
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:32:20 +0300

    Create DataSet1.cs

---
+++ SQL_Table_Stars/SQL_Table_Stars/DataSet1.cs
@@ -0,0 +1,31 @@
+﻿namespace SQL_Table_Stars
+{
+
+
+    partial class DataSet1
+    {
+        partial class PathsDataTable
+        {
+        }
+
+        partial class FieldsDataTable
+        {
+        }
+
+        partial class TablesDataTable
+        {
+        }
+
+        partial class TableField_PathsDataTable
+        {
+        }
+
+        partial class Table_Field_PathDataTable
+        {
+        }
+
+        partial class PK_FKDataTable
+        {
+        }
+    }
+}
