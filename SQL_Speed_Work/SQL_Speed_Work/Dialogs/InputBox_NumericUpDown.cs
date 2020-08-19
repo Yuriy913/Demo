b6a46c72423ed52c3cf5e5c579f66ccee62fae95
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:16:15 +0300

    Create InputBox_NumericUpDown.cs

---
+++ SQL_Speed_Work/SQL_Speed_Work/Dialogs/InputBox_NumericUpDown.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQL_Speed_Work.Dialogs
+{
+    public partial class InputBox_NumericUpDown : Form
+    {
+        public string Caption = "";
+        public decimal Value = -1;
+        public InputBox_NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Value = numericUpDown1.Value;
+            Close();
+        }
+
+        private void InputBox_NumericUpDown_Load(object sender, EventArgs e)
+        {
+            l_Caption.Text = Caption;
+        }
+    }
+}
