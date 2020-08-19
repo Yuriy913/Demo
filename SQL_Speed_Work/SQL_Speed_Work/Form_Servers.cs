4624dc53300b07c7fa3fd0cc37a05747a104077d
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:16:29 +0300

    Create Form_Servers.cs

---
+++ SQL_Speed_Work/SQL_Speed_Work/Form_Servers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQL_Speed_Work
+{
+    public partial class Form_Servers : Form
+    {
+        public Form_Servers()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            dataSet101.EndInit();
+        }
+    }
+}
