6a7ed87d28e349f105eacf91451f524f44d5adcf
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 14:51:09 +0300

    Create PreviewWindow.aspx.cs

---
+++ WebSite/Clients/Alliance/PreviewWindow.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Clients_Alliance_PreviewWindow : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		int alertID = 0;
+
+		if (Request.QueryString["ID"] != null)
+		{
+			Int32.TryParse(Request.QueryString["ID"], out alertID);
+		}
+
+		string messageText = String.Empty;
+
+		if (alertID != 0)
+		{
+			try
+			{
+				messageText = Data.getAllianceAlertText(alertID);
+			}
+			catch //(Exception ex)
+			{
+				//Data.ShowError(pInfo, ex);
+			}
+		}
+
+		if (!messageText.Equals(String.Empty))
+		{
+			Response.Write(messageText);//load it to current window
+			Response.End();
+		}
+	}
+}
