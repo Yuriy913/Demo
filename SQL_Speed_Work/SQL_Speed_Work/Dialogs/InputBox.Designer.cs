11fa23ab882c77840901aabc8df545ffb39482f6
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:16:11 +0300

    Create InputBox.Designer.cs

---
+++ SQL_Speed_Work/SQL_Speed_Work/Dialogs/InputBox.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SQL_Speed_Work.Dialogs
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l_Caption = new System.Windows.Forms.Label();
+            this.tB_Value = new System.Windows.Forms.TextBox();
+            this.bOK = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // l_Caption
+            // 
+            this.l_Caption.AutoSize = true;
+            this.l_Caption.Location = new System.Drawing.Point(13, 9);
+            this.l_Caption.Name = "l_Caption";
+            this.l_Caption.Size = new System.Drawing.Size(35, 13);
+            this.l_Caption.TabIndex = 0;
+            this.l_Caption.Text = "label1";
+            // 
+            // tB_Value
+            // 
+            this.tB_Value.Location = new System.Drawing.Point(16, 25);
+            this.tB_Value.Name = "tB_Value";
+            this.tB_Value.Size = new System.Drawing.Size(217, 20);
+            this.tB_Value.TabIndex = 1;
+            // 
+            // bOK
+            // 
+            this.bOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bOK.Location = new System.Drawing.Point(30, 51);
+            this.bOK.Name = "bOK";
+            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.TabIndex = 2;
+            this.bOK.Text = "OK";
+            this.bOK.UseVisualStyleBackColor = true;
+            this.bOK.Click += new System.EventHandler(this.bOK_Click);
+            // 
+            // bCancel
+            // 
+            this.bCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bCancel.Location = new System.Drawing.Point(130, 51);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.TabIndex = 3;
+            this.bCancel.Text = "Cancel";
+            this.bCancel.UseVisualStyleBackColor = true;
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 93);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.bOK);
+            this.Controls.Add(this.tB_Value);
+            this.Controls.Add(this.l_Caption);
+            this.Name = "InputBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InputBox";
+            this.Load += new System.EventHandler(this.InputBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_Caption;
+        private System.Windows.Forms.TextBox tB_Value;
+        private System.Windows.Forms.Button bOK;
+        private System.Windows.Forms.Button bCancel;
+    }
+}
