e07ad9638e1a24f963688153e9147d9801d99fed
Yury Parshyn <yury_parshyn@epam.com>
2020-08-19 15:31:20 +0300

    Create sqlTemplatesDataSet.cs

---
+++ SQL_Speed_Work/SQL_Speed_Work/sqlTemplatesDataSet.cs
@@ -0,0 +1,14 @@
+﻿namespace SQL_Speed_Work
+{
+
+
+    public partial class sqlTemplatesDataSet
+    {
+    }
+}
+namespace SQL_Speed_Work {
+    
+    
+    public partial class sqlTemplatesDataSet {
+    }
+}
